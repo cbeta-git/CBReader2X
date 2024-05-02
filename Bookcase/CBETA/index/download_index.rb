4123741a28bb15ffbf3293b8129868571fbfe10c
heavenchou <heaven.chou@gmail.com>
2024-05-03 05:50:04 +0800

    更新索引檔下載程式

--- Bookcase/CBETA/index/download_index.rb
+++ Bookcase/CBETA/index/download_index.rb
@@ -29,18 +29,19 @@ lines.each { |line|
 
         # 先處理大檔的警告
         if file_size > 5000000
-            html = URI.open(file_url).read
+            puts "檔案較大，請耐心稍候..."
+            # html = URI.open(file_url).read
 
             # <form id="download-form" action="https://drive.google.com/uc?id=10fhAIiIRTHuj08Z89GtJPCiWuqLeaP-3&amp;export=download&amp;format=application/octet-stream&amp;confirm=t&amp;uuid=eb2c2b64-b67e-4bd2-92e1-54fea44248f7&amp;at=AB6BwCC9ZjULf1so4hK9d_ET2rcd:1703312632877" method="post"><input type="submit" id="uc-download-link" class="goog-inline-block jfk-button jfk-button-action" value="仍要下載"></form>
 
-            if html.match(/action="(.*?)"/)
-                html = $1
-                html.gsub!('&amp;','&')
-                file_url = html
-                puts "檔案較大，請耐心稍候..."
-            else
-                puts "格式不太對勁，請檢查結果是否正確。"
-            end
+            # if html.match(/action="(.*?)"/)
+            #     html = $1
+            #     html.gsub!('&amp;','&')
+            #     file_url = html
+            #     puts "檔案較大，請耐心稍候..."
+            # else
+            #     puts "格式不太對勁，請檢查結果是否正確。"
+            # end
         end
 
         # 下載檔案
